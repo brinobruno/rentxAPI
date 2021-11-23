1482a9f2df5dc08e53f816f5b716f4626c4ba9a5
Bruno Corrêa <brunosantos6ft@gmail.com>
2021-11-23 09:55:44 -0300

    debug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 
+import { CreateCategoryController } from "src/modules/cars/useCases/createCategory/CreateCategoryController";
+
 const router = Router()
 
 router.use('/categories', categoriesRoutes)
